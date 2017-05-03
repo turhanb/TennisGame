ce24fccad1858edcb6a487ea0490ccc682c316da
Itir Karac <karacitir@gmail.com>
2017-05-03 12:40:02 +0300

    method templates added

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,4 +1,35 @@
 
 public class TennisGame {
+public TennisGame() {
+		// TO BE IMPLEMENTED
+	}
+	
 
+	public void player1Scored() throws TennisGameException {
+		// To be implemented
+	}
+	
+	public void player2Scored() throws TennisGameException {
+		// TO BE IMPLEMENTED
+		}
+	
+	public String getScore() {
+		// Here is the format of the scores: "player1Score - player2Score"
+		// "0 - 0"
+		// "15 - 15"
+		// "30 - 30"
+		// "deuce"
+		// "15 - 0", "0 - 15"
+		// "30 - 0", "0 - 30"
+		// "40 - 0", "0 - 40"
+		// "30 - 15", "15 - 30"
+		// "40 - 15", "15 - 40"
+		// "advantage player1"
+		// "advantage player2"
+		// "game player1"
+		// "game player2"
+		
+		// TO BE IMPLEMENTED
+		return "";
+	}
 }
